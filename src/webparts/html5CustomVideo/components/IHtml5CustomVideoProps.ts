a25c6cef9b0a287ccb40291a86841370cd5fbcff
RazvanHrestic <noreply@arag.de>
2026-02-25 10:13:53 +0100

    Basic spfx webpart scaffolding

---
+++ src/webparts/html5CustomVideo/components/IHtml5CustomVideoProps.ts
@@ -0,0 +1,7 @@
+export interface IHtml5CustomVideoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/html5CustomVideo/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHtml5CustomVideoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'Html5CustomVideoWebPartStrings' {
+  const strings: IHtml5CustomVideoWebPartStrings;
+  export = strings;
+}
